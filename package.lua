14f0c52d6a7faf0fd93e48117ef1260ad16994bb
Tim Caswell <tim@creationix.com>
2015-04-06 20:20:47 -0700

    Use luvi 1.1.0

--- package.lua
+++ package.lua
@@ -1,7 +1,10 @@
 return {
   name = "creationix/gamepad-sample",
   version = "0.0.0",
-  luvi = { flavor = "tiny" },
+  luvi = {
+     flavor = "tiny",
+     version = "1.1.0",
+  },
   dependencies = {
     "luvit/pretty-print",
     "luvit/require",
